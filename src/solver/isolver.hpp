1b7a8e83ca2ebf5a6d3cd243cafbdc4d9508c38d
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2015-09-14 12:55:58 +0700

    solver and expr interfaces

---
+++ src/solver/isolver.hpp
@@ -0,0 +1,14 @@
+
+namespace Solver
+{
+  /**
+ * ISolver is an abstract solver interface.
+ * @author Ivan Yakimov, e-mail: ivan.yakimov.research@yandex.ru
+ * @date 14.09.2015
+ *
+ */
+  class ISolver
+  {
+    virtual ISolver () = 0;
+  };
+}
