181f49ec927d9547594cdcabe07941e6c311b0bc
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2015-10-15 15:05:31 +0700

    redundant printing from list<Instruction*> removed

--- src/parser/func-printer.cpp
+++ src/parser/func-printer.cpp
@@ -11,23 +11,6 @@ bool FuncPrinter::runOnFunction (Function &F)
   // Visit instructions
   InstPrinter inst_printer;
   inst_printer.visit (F);
-
-  // ----------------------------------------
-  // TODO: remove, this is just for checking the instruction visitors
-  std::set <Instruction*> WorkList;
-  for (inst_iterator i = inst_begin (F), e = inst_end (F); i != e; i++)
-    WorkList.insert (&*i);
-
-  // it is very strange, but perhaps this loop visits instructions in incorrect order o0 !?..
-  while (!WorkList.empty ())
-    {
-      // todo: apply Instruction Visitors http://llvm.org/doxygen/InstVisitor_8h-source.html
-      // or just get operands http://stackoverflow.com/questions/8651829/getting-the-operands-in-an-llvm-instruction
-      Instruction *I = *WorkList.begin ();
-      WorkList.erase (WorkList.begin ());
-      // /*disabled */ errs ().write_escaped (I->getOpcodeName ()) << "\n";
-    }
-  // ----------------------------------------
     
   // No transformations.
   return false;
