540ceb7fbd90beda9706e4c752544049f092f6d8
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2015-11-18 11:28:16 +0700

    solver test support

---
+++ build/solver.test.mk
@@ -0,0 +1,24 @@
+# useful links:
+# https://www.gnu.org/software/make/manual/html_node/Pattern-Rules.html
+# https://www.gnu.org/prep/standards/html_node/Makefile-Conventions.html
+SHELL = /bin/sh
+
+SOLVERTEST = expr-test.o 
+CXX = g++ 
+CXXFLAGS = -fdiagnostics-color=always -O0 -ggdb -fexceptions -std=c++1y -fpic
+LDFLAGS = -lgtest -pthread 
+
+vpath %.cpp ../test/solver/
+vpath %.hpp ../test/solver/
+
+solvertest.out: $(SOLVERTEST)
+	$(CXX) $< -o $@ $(LDFLAGS) $(CSSFLAGS)
+
+$(SOLVERTEST):
+%.o: %.cpp %.hpp
+	$(CXX) -c $< -o $@ $(CXXFLAGS)
+
+print: *.hpp *.cpp
+	lpr $?
+	touch $@
+
