bcf677fc7e8d6b9af50ef6f2039efeec531c6b96
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2015-11-19 14:58:09 +0700

    printing tests complete

--- test/solver/test-expr.cpp
+++ test/solver/test-expr.cpp
@@ -1,8 +1,15 @@
 // useful links:
 // https://github.com/google/googletest/blob/master/googletest/docs/Primer.mdy
 
+// Project
 # include "../../src/solver/expr.hpp"
+
+// Google Test
 # include "gtest/gtest.h"
+
+// STL
+# include <memory>
+using std::shared_ptr; using std::make_shared;
 
 namespace {
 class ExprTest : public ::testing::Test {
@@ -12,7 +19,22 @@ class ExprTest : public ::testing::Test {
 
 TEST_F(ExprTest, PrintVariable) {
 	solver::Variable v("x");
+	//TODO: check operands order
 	EXPECT_EQ("x", v.ToString());
+}
+
+TEST_F(ExprTest, PrintConstant) {
+	solver::ConstantI32 c(28);
+	//TODO: check operands order
+	EXPECT_EQ("28", c.ToString());
+}
+
+TEST_F(ExprTest, PrintAdd) {
+	auto l = make_shared<solver::Variable>("x");
+	auto r = make_shared<solver::Variable>("y");
+	solver::BinaryOperation add(l, r, solver::BinaryOperation::OpCode::kAdd);
+	//TODO: check operands order
+	EXPECT_EQ("add x y", add.ToString());
 }
 
 int main (int argc, char *argv[]) {
