a6923c21ae9bdd1fdf95ab9ae7a118cb833aed13
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2015-11-20 15:02:40 +0700

    interpreter implementation (initial)

---
+++ src/parser/interpreter.hpp
@@ -0,0 +1,12 @@
+# ifndef __INTERPRETER_HPP__
+# define __INTERPRETER_HPP__
+
+// Project
+# include "pattern-matcher.hpp"
+
+class Interpreter final : public PatternMatcher
+{
+
+};
+
+# endif /* __INTERPRETER_HPP__ */
