550d332742387f36788da13e87cfa262e9f8de8a
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2015-11-20 11:20:15 +0700

    PrintExprTest class refactoring

--- test/solver/test-expr.cpp
+++ test/solver/test-expr.cpp
@@ -9,29 +9,33 @@
 
 // STL
 # include <memory>
-using std::shared_ptr; using std::make_shared;
+# include <string>
+# include <iostream>
 
 namespace {
-class ExprTest : public ::testing::Test {
-
+class PrintExprTest : public ::testing::Test {
+public:
+	std::shared_ptr<solver::Variable> MakeVar(std::string name) {
+		return std::make_shared<solver::Variable>(name);
+	}
 };
 }
 
-TEST_F(ExprTest, PrintVariable) {
+TEST_F(PrintExprTest, Variable) {
 	solver::Variable v("x");
 	//TODO: check operands order
 	EXPECT_EQ("x", v.ToString());
 }
 
-TEST_F(ExprTest, PrintConstant) {
+TEST_F(PrintExprTest, Constant) {
 	solver::ConstantI32 c(28);
 	//TODO: check operands order
 	EXPECT_EQ("28", c.ToString());
 }
 
-TEST_F(ExprTest, PrintAdd) {
-	auto l = make_shared<solver::Variable>("x");
-	auto r = make_shared<solver::Variable>("y");
+TEST_F(PrintExprTest, BinaryOp) {
+	auto l = MakeVar("x");
+	auto r = MakeVar("y");
 	solver::BinaryOperation add(l, r, solver::BinaryOperation::OpCode::kAdd);
 	//TODO: check operands order
 	EXPECT_EQ("add x y", add.ToString());
@@ -41,4 +45,3 @@ int main (int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
-
