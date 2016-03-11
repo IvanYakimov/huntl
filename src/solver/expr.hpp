3aad41daf167636b9df53c3f0fb0d3408ed313e6
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-03-11 18:39:30 +0700

    [solver docs] draft of Expr's hierachy docs

--- src/solver/expr.hpp
+++ src/solver/expr.hpp
@@ -29,22 +29,35 @@ namespace solver
 	using VarPtr = std::shared_ptr<Var>;
 	using BinOpPtr = std::shared_ptr<BinOp>;
 
-	//TODO: total refactoring
+	/**
+	 * A basic SMT expression. Every particular kind of expression should be inherited (by using CRTP<T,B>) from this.
+	 */
 	class Expr : public CRTP<Expr, Object> {
 	public:
 		virtual ~Expr() {}
 	};
 
+	/**
+	 * An arbitrary binary operation. Kind of operation is a class field and set up for every particular class instance in rut-time.
+	 */
 	class BinOp : public CRTP<BinOp, Expr>{
 	public:
-		//TODO: place Kind at first position
+		/** Basic constructor, do NOT use it directly! Use ::solver::ExprManager::MkBinOp() instead */
 		BinOp(ExprPtr left_child, ExprPtr right_child, Kind kind);
 		~BinOp();
+		/** Structural equality of this BinOp instance and another object instance. Returns true if rhs is instance of BinOp,
+		 * it has equivalent kind and their left and right children are both structurally equivalent. */
 		bool Equals(const Object &rhs) const final;
+		/** String representation in format "(kind left right)", where kind - string representation of the binop's kind,
+		 * left and right - string representation of the binop's children*/
 		std::string ToString() const final;
+		/** Returns (smart) pointer to left children */
 		ExprPtr GetLeftChild() const;
+		/** Returns (smart) pointer to right children */
 		ExprPtr GetRightChild() const;
+		/** Returns kind of the binop \see ::solver::Kind */
 		Kind GetKind() const;
+		/** Returns an appropriate string representation of the binop's kind */
 		std::string GetKindName() const;
 	private:
 		ExprPtr left_child_;
@@ -52,25 +65,45 @@ namespace solver
 		Kind kind_;
 	};
 
-	//TODO: add variable type
+	/**
+	 * A variable (constant in terms of SMT-LIB2).
+	 */
 	class Var final : public CRTP <Var, Expr> {
 	public:
-		Var (std::string name, TypePtr type) throw(std::logic_error); //TODO: throw
+		/** Basic constructor, do NOT use it directly! Use ::solver::ExprManager::MkVar() instead */
+		Var (std::string name, TypePtr type) throw(std::logic_error);
 		virtual ~Var() final;
+		/** Structural equality of this Var instance and another Object instance. Returns true if rhs is instance of Var
+		 * and it has the same type as this.
+		 */
 		virtual bool Equals(const Object& rhs) const final;
+		/** String representation of the variable. Returns string in format "type name",
+		 * where type - string representation of a variable type and name - a name of the variable */
 		virtual std::string ToString() const final;
+		/** Returns name of the variable */
 		std::string GetName() const;
+		/** Returns (smart) pointer to a variable type */
 		TypePtr GetType() const;
 	private:
 		std::string name_;
 		TypePtr type_;
 	};
 
+	/**
+	 * A constant.
+	 */
 	class Const : public CRTP<Const, Expr> {
 	public:
+		/** Basic constructor, do NOT use it directly! Use ::solver::ExprManager::MkConst() instead */
 		Const (ValuePtr val) throw(std::logic_error);
 		virtual ~Const();
+		/** Structural equality of this Const instance and another Object instance. Returns true if Object is instance of Const
+		 * and their values are structurally equivalent.
+		 */
 		virtual bool Equals(const Object& rhs) const final;
+		/** String representation of the constant. Format is "value", where
+		 * value is a string representation of the constant's value.
+		 */
 		virtual std::string ToString() const final;
 		ValuePtr GetValue() const;
 	private:
