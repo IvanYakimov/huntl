9935aa5c1b0f45440f63cea812bae45336194bf6
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-03-11 19:02:08 +0700

    [solver docs] docs for Value - in progress

--- src/solver/value.hpp
+++ src/solver/value.hpp
@@ -20,19 +20,37 @@ namespace solver {
 	using BasicIntPtr = std::shared_ptr<BasicInt>;
 	template<typename T> using IntPtr = std::shared_ptr<Int<T>>;
 
+	/** Basic value. Every particular value class should be inherited (by CRTP<T,B>) from this.
+	 * \see BasicInt
+	 */
 	class Value : public CRTP <Value, Object> {
 	public:
 		virtual ~Value() {}
 	};
 
+	/** Basic integer value. This is useful to make (smart) pointer for particular integer value
+	 * \see Int
+	 */
 	class BasicInt : public CRTP <BasicInt, Value> {
 	public:
+		/** Returns width (number of bits) in the integer. */
 		virtual Width GetWidth() const = 0;
+		/** Returns true if this is signed integer, and false if it's not. */
 		virtual bool IsSigned() const = 0;
+		/** Returns 64-bit-lenghted unsigned long representation of the stored integer value.
+		 * This routine copies significant bytes from value to result (in machine dependent order),
+		 * and fills insignificant bytes by zeros. */
 		virtual uint64_t GetUInt64() const = 0;
+		/** Set up value from 64-bit-lenghted unsigned long representation.
+		 * This routine copies significant bytes from argument to value (in machine dependent order),
+		 * and fills insignificant bytes by zeros. */
 		virtual void SetUInt64(const uint64_t& val) = 0;
 	};
 
+	/** Particular integer value.
+	 * \tparam T is a fixed width integer (scalar) type from STL. It can be:
+	 * int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t.
+	 */
 	template<typename T>
 	class Int : public CRTP <Int<T>, BasicInt> {
 	public:
