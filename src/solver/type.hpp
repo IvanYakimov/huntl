3bb8852c10d809591f245b6388349094854873f8
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-03-12 09:57:43 +0700

    [solver docs] Type docs done

--- src/solver/type.hpp
+++ src/solver/type.hpp
@@ -16,26 +16,44 @@ namespace solver {
 
 	using TypePtr = std::shared_ptr<Type>;
 
+	/** Basic type. Every particular type should be inherited (bt CRTP<T,B>) from this. */
 	class Type : public CRTP <Type, Object> {
 	public:
 		virtual ~Type() {}
 	};
 
+	/** Basic integer type. Is is useful to make (smart) pointer to particular IntTy.
+	 * \see IntTy
+	 * \see ExprManager::MkIntTy */
 	class BasicIntTy : public CRTP <BasicIntTy, Type> {
 	public:
 		virtual ~BasicIntTy() {}
+		/** Returns width (number of bites) of wrapped raw integer type */
 		virtual Width GetWidth() const = 0;
+		/** Returns true if wrapped raw integer type is signed, false if it's not */
 		virtual bool IsSigned() const = 0;
 	};
 
+	/** Particular integer type.
+	  * \tparam T is a fixed width integer (scalar) type from STL. It can be:
+	 * int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t.
+	 */
 	template<typename T>
 	class IntTy : public CRTP <IntTy<T>, BasicIntTy> {
 	public:
+		/** Basic constructor, do NOT use int directly! Use ExprManager::MkIntTy instead */
 		IntTy ();
 		virtual ~IntTy();
+		/** Structural equality of this instance of IntTy and instance of another Object.
+		 * Returns true if rhs is instance of IntTy
+		 * and if template parameters T are equivalent.
+		 */
 		virtual bool Equals (const Object& rhs) const final;
+		/** String representation of integer type in format "i<width>", where width = number of bites of wrapped raw integer type. */
 		virtual std::string ToString() const final;
+		/** Number of bitest, \see BasicIntTy::GetWidth */
 		virtual Width GetWidth() const final;
+		/** Is it signed? \see BasicIntTy::IsSigned */
 		virtual bool IsSigned() const final;
 	};
 
