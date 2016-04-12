569740696f78c78a9b6d5e74f7f37e41f56b970a
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-04-12 14:50:06 +0700

    [interpreter] pattern matcher test started

---
+++ test/interpreter/test-pattern-matcher.cpp
@@ -0,0 +1,16 @@
+#include "../../src/interpreter/pattern-matcher.hpp"
+
+// Google Test
+#include "gtest/gtest.h"
+
+namespace interpreter {
+	class MatcherTest : public ::testing::Test {
+	};
+
+	TEST_F(MatcherTest, dummy) {
+
+	}
+}
+
+
+
