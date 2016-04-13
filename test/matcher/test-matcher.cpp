0c7f958b5250e9195d79976932c5837ea28283a1
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-04-13 17:07:32 +0700

    [matcher test] ret const case implementation started

--- test/matcher/test-matcher.cpp
+++ test/matcher/test-matcher.cpp
@@ -27,6 +27,27 @@ TEST_F(MatcherTest, bodyless_function) {
 	delete module_;
 }
 
+TEST_F(MatcherTest, ret_const) {
+	module_ = new Module("test", context_);
+	FunctionType *func_ty = FunctionType::get(Type::getVoidTy(context_), false);
+	Function *func = Function::Create(func_ty, Function::ExternalLinkage, "test", module_);
+	BasicBlock *entry_ = BasicBlock::Create(context_, "entry", func);
+	builder_.SetInsertPoint(entry_);
+
+	/**/
+	auto c1 = ConstantInt::get(module_->getContext(), APInt(32, 2, true));
+	//auto c2 = ConstantInt::get(module_->getContext(), APInt(32, 2, true));
+	//auto add = builder_.CreateAdd(c1, c2, "addtmp");
+	builder_.CreateRet(c1);
+	/**/
+
+	auto done = verifyFunction(*func);
+	ASSERT_TRUE(done);
+	errs() << *func << "\n";
+	delete module_;
+}
+
+
 int main(int argc, char** argv, char **env) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
