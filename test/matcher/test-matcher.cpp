8c6a50ed5ef06117b290e4a3b23ef321158d6e11
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-04-13 16:42:04 +0700

    [matcher test] bodyless function function initiation done

--- test/matcher/test-matcher.cpp
+++ test/matcher/test-matcher.cpp
@@ -4,10 +4,23 @@
 
 using namespace llvm;
 
-int main(int argc, char** argv) {
-	llvm::LLVMContext &Context = llvm::getGlobalContext();
-	llvm::Module *TheModule;
-	llvm::IRBuilder<> Builder (llvm::getGlobalContext());
+LLVMContext &context_ = getGlobalContext();
+Module *module_;
+IRBuilder<> builder_ (getGlobalContext());
+
+void InitModule() {
+	module_ = new Module("test", context_);
+	FunctionType *func_ty = FunctionType::get(Type::getVoidTy(getGlobalContext()), false);
+	Function *func = Function::Create(func_ty, Function::ExternalLinkage, "test", module_);
+	BasicBlock *entry_ = BasicBlock::Create(getGlobalContext(), "entry", func);
+	builder_.SetInsertPoint(entry_);
+	errs() << verifyFunction(*func) << "\n";
+	errs() << *func << "\n";
+	delete module_;
+}
+
+int main(int argc, char** argv, char **env) {
+	InitModule();
 	return 0;
 }
 
