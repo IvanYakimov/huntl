78d1ef1911e036f949817328c08be2c532fc6224
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-04-19 16:50:01 +0700

    [matcher test] refactoring - function extracted from the MatcherTest

--- test/matcher/test-matcher.cpp
+++ test/matcher/test-matcher.cpp
@@ -119,16 +119,20 @@ public:
 	Type* Int1Ty() {
 		return Type::getInt1Ty(context_);
 	}
+
+	Type* VoidTy() {
+		return Type::getVoidTy(context_);
+	}
 };
 
 class Int32Func : public Func {
 public:
-	Int32Func(const char* name = "f") : Func(FunctionType::getInt32Ty(getGlobalContext()), name) {}
+	Int32Func(const char* name = "f") : Func(FunctionType::get(Type::getInt32Ty(getGlobalContext()), false), name) {}
 };
 
 class VoidFunc : public Func {
 public:
-	VoidFunc(const char* name = "f") : Func(FunctionType::getVoidTy(getGlobalContext()), name) {}
+	VoidFunc(const char* name = "f") : Func(FunctionType::get(Type::getVoidTy(getGlobalContext()), false), name) {}
 };
 
 //TODO: total refactoring!!!
@@ -137,7 +141,8 @@ public:
 	interpreter::MatcherStub matcher_;
 
 	// Matching
-	void MatchOnFunc(Function* f) {
+	void Match(Func &func) {
+		Function* f = func.Get();
 		errs() << *f << "\n";
 		ASSERT_FALSE(verifyFunction(*f));
 		// If there are no errors, the function returns false.
@@ -148,77 +153,89 @@ public:
 };
 
 TEST_F(MatcherTest, ret__void) {
-	VoidFunc f;
-	auto b0 = f.Block("b0");
-	f.Enter(b0);
-	auto ret = f.RetVoid();
+	VoidFunc f; {
+		auto ret = f.RetVoid();
+	}
+	Match(f);
 }
 
 TEST_F(MatcherTest, ret__const) {
-	Int32Func f;
-	auto ret = f.Ret(f.I32(42));
+	Int32Func f; {
+		auto ret = f.Ret(f.I32(42));
+	}
+	Match(f);
 }
 
 TEST_F(MatcherTest, alloca_store_load_ret) {
-	Int32Func f;
-	auto x = f.Alloca32("x");
-	auto store_x = f.Store(f.I32(2), x);
-	auto load_x = f.Load(x);
-	auto ret = f.Ret(load_x);
+	Int32Func f; {
+		auto x = f.Alloca32("x");
+		auto store_x = f.Store(f.I32(2), x);
+		auto load_x = f.Load(x);
+		auto ret = f.Ret(load_x);
+	}
+	Match(f);
 }
 
 TEST_F(MatcherTest, if_than_else) {
-	Int32Func f;
-	auto true_branch = f.Block("true-branch");
-	auto false_branch = f.Block("false-branch");
-	auto x = f.Alloca32("x");
-	auto icmp = f.EQ(f.Load(x), f.I32(2));
-	auto jump = f.IfThanElse(icmp, true_branch, false_branch);
-	f.Enter(true_branch); {
-		auto ret = f.Ret(f.I32(1));
+	Int32Func f; {
+		auto true_branch = f.Block("true-branch");
+		auto false_branch = f.Block("false-branch");
+		auto x = f.Alloca32("x");
+		auto icmp = f.EQ(f.Load(x), f.I32(2));
+		auto jump = f.IfThanElse(icmp, true_branch, false_branch);
+		f.Enter(true_branch); {
+			auto ret = f.Ret(f.I32(1));
+		}
+		f.Enter(false_branch); {
+			auto ret = f.Ret(f.I32(-1));
+		}
 	}
-	f.Enter(false_branch); {
-		auto ret = f.Ret(f.I32(-1));
-	}
+	Match(f);
 }
 
 TEST_F(MatcherTest, jump) {
-	Int32Func f;
-	auto dest = f.Block("dest");
-	auto jump = f.Jump(dest);
-	f.Enter(dest); {
-		f.Ret(f.I32(0));
+	Int32Func f; {
+		auto dest = f.Block("dest");
+		auto jump = f.Jump(dest);
+		f.Enter(dest); {
+			f.Ret(f.I32(0));
+		}
 	}
+	Match(f);
 }
 
 TEST_F(MatcherTest, binop) {
-	Int32Func f;
-	auto x = f.Alloca32("x");
-	auto y = f.Alloca32("y");
-	auto binop = f.Add(f.Load(x), f.Load(y));
-	auto ret = f.Ret(binop);
+	Int32Func f; {
+		auto x = f.Alloca32("x");
+		auto y = f.Alloca32("y");
+		auto binop = f.Add(f.Load(x), f.Load(y));
+		auto ret = f.Ret(binop);
+	}
+	Match(f);
 }
 
 TEST_F(MatcherTest, phi_node) {
-	Int32Func f;
-	auto more_cmp = f.Block("more_cmp");
-	auto phi_node = f.Block("phi_node");
-	auto x = f.Alloca32("x");
-	auto y = f.Alloca32("y");
-	auto cmp_x = f.NE(f.Load(x), f.I32(0));
-	auto br1 = f.IfThanElse(cmp_x, phi_node, more_cmp);
-	Value* cmp_y;
-	f.Enter(more_cmp); {
-		cmp_y = f.NE(f.Load(y), f.I32(0));
-		auto br2 = f.Jump(phi_node);
+	Int32Func f; {
+		auto more_cmp = f.Block("more_cmp");
+		auto phi_node = f.Block("phi_node");
+		auto x = f.Alloca32("x");
+		auto y = f.Alloca32("y");
+		auto cmp_x = f.NE(f.Load(x), f.I32(0));
+		auto br1 = f.IfThanElse(cmp_x, phi_node, more_cmp);
+		Value* cmp_y;
+		f.Enter(more_cmp); {
+			cmp_y = f.NE(f.Load(y), f.I32(0));
+			auto br2 = f.Jump(phi_node);
+		}
+		f.Enter(phi_node); {
+			PHINode* phi = f.Phi(f.Int1Ty());
+			phi->addIncoming(f.I1(true), f.Entry());
+			phi->addIncoming(cmp_y, more_cmp);
+			auto zext = f.ZExt(phi, f.Int32Ty());
+			auto ret = f.Ret(zext);
+		}
 	}
-	f.Enter(phi_node); {
-		PHINode* phi = f.Phi(f.Int1Ty());
-		phi->addIncoming(f.I1(true), f.Entry());
-		phi->addIncoming(cmp_y, more_cmp);
-		auto zext = f.ZExt(phi, f.Int32Ty());
-		auto ret = f.Ret(zext);
-	}
+	Match(f);
 }
 
 int main(int argc, char** argv, char **env) {
