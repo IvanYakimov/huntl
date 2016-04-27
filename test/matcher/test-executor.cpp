ff354f74d8f819f06d33ae356afdb22f67f7d319
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-04-27 12:15:00 +0700

    [executor test] basic definition

---
+++ test/matcher/test-executor.cpp
@@ -0,0 +1,1 @@
+#include "test-executor.hpp"
---
+++ test/matcher/test-executor.hpp
@@ -0,0 +1,6 @@
+#ifndef __TEST_EXECUTOR_HPP__
+#define __TEST_EXECUTOR_HPP__
+
+#include "ir-function-builder.hpp"
+
+#endif /* __TEST_EXECUTOR_HPP__ */
