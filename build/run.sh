89504327c422b404d8c6561a0042619de8c7d523
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-07-16 13:27:33 +0700

    run script improved (to take target name as an argument)

--- build/run.sh
+++ build/run.sh
@@ -1,1 +1,7 @@
-make program.so && rm -f test-gcd.ll && make test-gcd.ll && opt -load=./program.so < test-gcd.ll > /dev/null -ll-voyager
+#!/bin/bash
+target="$1"
+if [[ -z $target ]]; then
+	echo "target name is empty. format is './run <target-name>'" 
+else
+	make program.so && rm -f test-$target.ll && make test-$target.ll && opt -load=./program.so < test-$target.ll > /dev/null -ll-voyager
+fi
