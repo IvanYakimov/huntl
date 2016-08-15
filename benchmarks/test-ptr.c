0d77c21c5f3075d3eb1c21b746e142b504afc1ec
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-08-15 17:02:49 +0700

    ptr testing

--- benchmarks/test-ptr.c
+++ benchmarks/test-ptr.c
@@ -26,54 +26,26 @@ void test_ptrtoint() {
 	gen_ptrtoint_helper(a,r);
 }
 
-void twice_helper(int *x) {
-	*x = *x * 2;
-}
-
-int twice(int x) {
-	twice_helper(&x);
-	return x;
-}
-
-void gen_twice(int x, int r);
-
-void test_twice() {
-	int a = 28;
-	a = mksym_i32();
-	limit2_i32(&a, 0, 28);
-	int r = twice(a);
-	gen_twice(a,r);
-}
-
-void swap(int *x, int *y) {
+void gen_swap(int *a, int *b, int r);
+int swap(int *x, int *y) {
 	int tmp = *x;
 	*x = *y;
 	*y = tmp;
-}
-
-void gen_swap_tester(int a, int b, int r);
-
-int swap_tester(int a, int b) {
-	int a0 = a;
-	int b0 = b;
-	swap(&a,&b);
-	if (a == b0 && b == a0)
-		return 1;
-	else
-		return 0;
+	return 0;
 }
 
 void test_swap() {
 	int x = 28;
 	int y = 42;
+	int r = 0;
 	x = mksym_i32();
 	y = mksym_i32();
 	limit2_i32(&x, 0, 28);
 	limit2_i32(&y, 42, 101);
 	if (x == y)
 		x++;
-	int r = swap_tester(x,y);
-	gen_swap_tester(x,y,r);
+	r = swap(&x,&y);
+	gen_swap(&x,&y,r);
 }
 
 void gen_ptrarg(int *x, int res);
