bb1b60f75882b0c8e8f15203808b622159761d73
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-08-29 14:06:47 +0700

    lower by lower table

--- src/bigram-model.hpp
+++ src/bigram-model.hpp
@@ -6,10 +6,12 @@
 namespace interpreter {
 	class BigramModel {
 	public:
+		BigramModel();
 		static char UpperByUpper(char symbol);
 		static char LowerByLower(char symbol);
 		static char UpperByLower(char symbol);
 		static char LowerByUpper(char symbol);
+		float lower_by_lower[26][26];
 	};
 }
 
