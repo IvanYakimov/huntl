7a3f8b2a83bc265d3814a3b41a6d8af4b9172e77
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-09-02 17:32:33 +0700

    estimat - string matcher

--- readability/estimate.cpp
+++ readability/estimate.cpp
@@ -2,9 +2,19 @@
 
 using namespace interpreter;
 
+std::smatch GetMatch(std::string line) {
+	std::regex r("\"[a-zA-Z]*\"");
+	std::smatch match;
+	regex_search(line, match, r);
+	return match;
+}
+
 int main() {
-	std::string line;
 	for (std::string line; std::getline(std::cin, line); line.length() != 0) {
-		std::cout << line << std::endl;
+		//std::cout << "for: " << line << " -- ";
+		for (auto match = GetMatch(line); match.size() > 0; match = GetMatch(match.suffix())) {
+			//std::cout << "matched: " << match.str() << ", ";
+		}
+		//std::cout << std::endl;
 	}
 }
