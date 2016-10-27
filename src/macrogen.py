c86970d4e22a7bed92fd1714cb6622a3ca5e070e
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-10-27 10:44:10 +0700

    'C-templates' generator done

--- src/macrogen.py
+++ src/macrogen.py
@@ -1,54 +1,40 @@
 #!/usr/bin/env python
 import fileinput
 import sys
-#see: https://www.tutorialspoint.com/python/python_xml_processing.htm
 import xml.sax
 
+# see: https://interactivepython.org/runestone/static/pythonds/BasicDS/ImplementingaStackinPython.html
+class Stack:
+     def __init__(self): self.items = []
+     def isEmpty(self): return self.items == []
+     def push(self, item): self.items.append(item)
+     def pop(self): self.items.pop()
+     def top(self): return self.items[len(self.items)-1]
+     def size(self): return len(self.items)
+    
+tagStack = Stack()
+templateStack = Stack()
 filety = 0
 kDecl = 1
 kDef = 2
-argnum = len(sys.argv)
 
 def argsWarning():
     print "wrong number of arguments, use --decl of --def"
     sys.exit()
 
-if argnum  == 1:
-    argsWarning()
+argnum = len(sys.argv)
+if argnum  == 1: argsWarning()
 elif argnum == 2:
     arg1 = sys.argv[1]
-    if arg1 == "--decl":
-       filety = kDecl
-    elif arg1 == "--def":
-        filety = kDef
-    else:
-        argsWarning()
-else:
-    argsWarning()
-    
-# see: https://interactivepython.org/runestone/static/pythonds/BasicDS/ImplementingaStackinPython.html
-class Stack:
-     def __init__(self):
-         self.items = []
-     def isEmpty(self):
-         return self.items == []
-     def push(self, item):
-         self.items.append(item)
-     def pop(self):
-         return self.items.pop()
-     def top(self):
-         return self.items[len(self.items)-1]
-     def size(self):
-         return len(self.items)
-    
-tagStack = Stack()
-templateStack = Stack()
+    if arg1 == "--decl": filety = kDecl
+    elif arg1 == "--def": filety = kDef
+    else: argsWarning()
+else: argsWarning()
 
 # parser
 class TemplateHandler (xml.sax.ContentHandler):
-    def __init__(self):
-        key = ""
-
+    def __init__(self): pass
+    
     def startElement(self, tag, atts):
         tagStack.push(tag)
         if tag == "func":
@@ -58,66 +44,45 @@ class TemplateHandler (xml.sax.ContentHandler):
             exp = atts["exp"].strip().split()
             templateStack.push((id_, exp))
 
-    def title(self):
-        print "\t//" + self.name
+    def printTitle(self): print "\t//" + self.name
+    def printDeclaration(self, header): print "\t" + header + ";"
+    def printDefinition(self, header, body): print "\t" + header + "{\n\t\t" + body + "\n\t}\n"
+    def declareOrdinary(self, header, body): self.printDeclaration(header)
+    def defineOrdinary(self, header, body): self.printDefinition(header, body)
+    def declareTemplated(self, header, body, id_, e): self.printDeclaration(header.replace(id_, e))
+    def defineTemplated(self, header, body, id_, e): self.printDefinition(header.replace(id_, e), body.replace(id_, e))
 
-    def declare(self):
+    def handleOrdinary(self, handler):
         assert (templateStack.isEmpty())
-        print "\t" + self.header + ";"
+        handler(self.header, self.body)
 
-    def define(self):
-        assert (templateStack.isEmpty())
-        print "\t" + self.header + " {"
-        print "\t\t" + self.body + "\n\t}\n"
-
-    def handleTemplated(self):
-        assert (templateStack.size() == 1)
-
-    def declare_templated(self):
+    def handleTemplated(self, handler):
         assert (templateStack.size() == 1)
         template = templateStack.top()
         id_ = template[0]
         exp_ = template[1]
-        for e in exp_:
-            print "\t" + self.header.replace(id_, e) + ";"
-            
-    def define_templated(self):
-        assert (templateStack.size() == 1)
-        template = templateStack.top()
-        id_ = template[0]
-        exp_ = template[1]
-        for e in exp_:
-            print "\t" + self.header.replace(id_, e) + " {"
-            print "\t\t" + self.body.replace(id_, e) + "\n\t}\n"
+        for e in exp_: handler(self.header, self.body, id_, e)
             
     def endElement(self, tag):
         if tag == "func":
-            self.title()
+            self.printTitle()
             if not templateStack.isEmpty():
-                if filety == kDef:
-                    self.define_templated()
-                elif filety == kDecl:
-                    self.declare_templated()
-                else:
-                    sys.exit()
+                if filety == kDef: self.handleTemplated(self.defineTemplated)
+                elif filety == kDecl: self.handleTemplated(self.declareTemplated)
+                else: sys.exit()
             else:
-                if filety == kDef:
-                    self.define()
-                elif filety == kDecl:
-                    self.declare()
-                else:
-                    sys.exit()
-        if tag == "template":
-           templateStack.pop()     
+                if filety == kDef: self.handleOrdinary(self.defineOrdinary)
+                elif filety == kDecl: self.handleOrdinary(self.declareOrdinary)
+                else: sys.exit()
+        if tag == "template": templateStack.pop()     
         tagStack.pop()
             
     def characters(self, val):
         s = val.strip()
-        if tagStack.top() == "body":
-            self.body = s
-        elif tagStack.top() == "header":
-            self.header = s
-            
+        if tagStack.top() == "body": self.body = s
+        elif tagStack.top() == "header": self.header = s
+
+#see: https://www.tutorialspoint.com/python/python_xml_processing.htm
 parser = xml.sax.make_parser()
 parser.setFeature(xml.sax.handler.feature_namespaces,0)
 handler = TemplateHandler()
