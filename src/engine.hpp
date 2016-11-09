3a426e197fea5bfde36857c8e155c558d8e9cf62
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-11-09 17:44:53 +0700

    macrolang generator draft done

--- src/engine.hpp
+++ src/engine.hpp
@@ -6,4 +6,10 @@
 template<typename T>
 void Alloca(Ref res, T allocated);
 
+template<typename T>
+void Binop(Ref res, OpCode code, Flag flag, Ref arg1, T op1, Ref arg2, T op2);
+
+template<typename T>
+void ICmp(Ref res, Cond cond, Ref op1, T v1, Ref op2, T v2);
+
 #endif
